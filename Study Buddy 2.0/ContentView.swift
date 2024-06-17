a2817fa1c11938dd476d17db4078f81dce017429
journeytd10 <journeytd10@gmail.com>
2024-06-17 09:17:28 -0400

    Initial Commit

---
+++ Study Buddy 2.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Study Buddy 2.0
+//
+//  Created by Journey Galore on 6/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Study Buddy 2.0/Study_Buddy_2_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Study_Buddy_2_0App.swift
+//  Study Buddy 2.0
+//
+//  Created by Journey Galore on 6/17/24.
+//
+
+import SwiftUI
+
+@main
+struct Study_Buddy_2_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
